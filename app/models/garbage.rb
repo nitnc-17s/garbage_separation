0ecd92647ffebd14607540bd3f545056e08f7d85
Crow314 <master@crow31415.net>
2021-07-15 16:03:09 +0900

    Create garbage register process

--- app/models/garbage.rb
+++ app/models/garbage.rb
@@ -9,4 +9,23 @@ class Garbage < ApplicationRecord
   validates :name,  uniqueness: { scope: :city }
   validates :index, presence: true
   validates :index, uniqueness: { scope: :city }
+
+  class << self
+    def register_garbage(name, city_name, category_name, separation_name)
+      city = City.find_by(name: city_name) || City.create!(name: city_name)
+      category = Category.find_by(name: category_name) || Category.create!(name: category_name)
+      separation = Separation.find_by(name: separation_name, city_id: city.id) \
+        || city.separations.create!(name: separation_name)
+
+      city.garbages.create!(name: name, index: Garbage.count, category: category, separation: separation)
+    end
+
+    def register_garbage!(name, city_name, category_name, separation_name)
+      city = City.find_by!(name: city_name)
+      category = Category.find_by!(name: category_name)
+      separation = Separation.find_by!(name: separation_name, city_id: city.id)
+
+      city.garbages.create!(name: name, index: Garbage.count, category: category, separation: separation)
+    end
+  end
 end
